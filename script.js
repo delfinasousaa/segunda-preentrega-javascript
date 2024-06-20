68d1eacc1708efab6c25a7422f63d39dfcf9f2c2
Delfina Sousa <sousadelfina02@gmail.com>
2024-06-20 15:28:42 -0300

    metodos

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function DatosUsuario() {
-    let usuarios = [];
 
+
+    let usuarios = [];
     let usuario = {
         nombre: "",
         edad: 0,
@@ -9,110 +10,107 @@ function DatosUsuario() {
         cantidadEntradas: 0,
         mail: "",
         fecha: "",
-
+    
         obtenerNombre: function() {
             this.nombre = prompt("Ingresa tu nombre");
-            console.log("Hola " + this.nombre);
+            alert("Hola " + this.nombre);
         },
-
+    
         validarEdad: function() {
             let edadUsuario = parseInt(prompt("Ingresa tu edad"));
-
+    
             while (isNaN(edadUsuario) || edadUsuario <= 0) {
-                console.log("Dato de edad inválido");
+                alert("Dato de edad inválido");
                 edadUsuario = parseInt(prompt("Ingresa tu edad"));
             }
-
             this.edad = edadUsuario;
-
+            
             if (this.edad >= 18) {
-                console.log("¡Bienvenida/o " + this.nombre + "!");
-            } 
-            else {
-                console.log("Lo sentimos, no eres mayor de edad...");
+                alert("¡Bienvenida/o " + this.nombre + "!");
+            } else {
+                alert("Lo sentimos, no eres mayor de edad...");
             }
         },
-
+    
         obtenerCiudad: function() {
             while (this.ciudadUsuario === "BARCELONA" && this.ciudadUsuario === "MADRID") {
                 this.ciudadUsuario = prompt("Ingresa tu ciudad: BARCELONA o MADRID").toUpperCase();
-
                 if (this.ciudadUsuario === "BARCELONA" || this.ciudadUsuario === "MADRID") {
                     break;
                 } 
                 else {
-                    console.log("Ciudad inválida, inténtalo de nuevo");
+                    alert("Ciudad inválida, inténtalo de nuevo");
                 }
             }
         },
     
         tipoDeEntrada: function() {
             this.tipoEntradas = prompt("Ingresa el tipo de entrada GENERAL o VIP").toUpperCase();
-
+    
             while (this.tipoEntradas !== "GENERAL" && this.tipoEntradas !== "VIP") {
-                console.log("Entrada no disponible");
+                alert("Entrada no disponible");
                 this.tipoEntradas = prompt("Ingresa el tipo de entrada GENERAL o VIP").toUpperCase();
             }
         },
-
+    
         cantidadDeEntradas: function() {
             let intentoEntradas = 0;
-
+    
             while (intentoEntradas < 3) {
                 this.cantidadEntradas = parseInt(prompt("Ingresa la cantidad de entradas (límite de 20 entradas por persona)"));
-
+    
                 if (!isNaN(this.cantidadEntradas) && this.cantidadEntradas > 0 && this.cantidadEntradas <= 20) {
                     return;
                 } else {
-                    console.log("Número de entradas inválido");
+                    alert("Número de entradas inválido");
                     intentoEntradas += 1;
                 }
                 if (intentoEntradas === 3) {
-                    console.log("Has alcanzado el máximo de intentos para ingresar el número de entradas");
+                    alert("Has alcanzado el máximo de intentos para ingresar el número de entradas");
                     return;
                 }
             }
         },
-
+    
         calcularEntradas: function() {
             let total = 0;
-
+    
             if (this.tipoEntradas === "GENERAL") {
                 total = 40 * this.cantidadEntradas;
             } 
             else if (this.tipoEntradas === "VIP") {
                 total = 100 * this.cantidadEntradas;
             }
-
-            console.log("El total a pagar es de: " + total + "€");
+    
+            alert("El total a pagar es de: " + total + "€");
         },
-
+    
         confirmarPago: function() {
             let abonar = prompt("¿Quieres abonar la entrada? SI (S) o NO (N)").toUpperCase();
-
+    
             if (abonar === "SI" || abonar === "S") {
                 this.contIntentos((intentosDeFecha) => {
                     this.fecha = prompt("Ingresa la fecha de las funciones disponibles: 05/06/24 o 10/06/24");
-
+    
                     if (this.fecha === "05/06/24" || this.fecha === "10/06/24") {
                         this.mail = prompt("Ingresa tu correo electrónico y se te enviará un enlace para realizar el pago");
-                        console.log("¡Gracias!");
+                        alert("¡Gracias!");
                         return false;
                     } 
                     else {
-                        console.log("Fecha incorrecta");
+                        alert("Fecha incorrecta");
                         return true;
                     }
                 });
             } 
             else if (abonar === "NO" || abonar === "N") {
-                console.log("Vuelve pronto :(");
+                alert("Vuelve pronto :(");
             } 
             else {
-                console.log("Respuesta no válida, vuelve a intentarlo más tarde...");
+                alert("Respuesta no válida, vuelve a intentarlo más tarde...");
             }
         },
-
+    
         contIntentos: function(procesoIntentos, maxIntentos = 3) {
             let intentos = 0;
             while (intentos < maxIntentos) {
@@ -121,28 +119,28 @@ function DatosUsuario() {
                 }
                 intentos += 1;
                 if (intentos === maxIntentos) {
-                    console.log("Has alcanzado el máximo de intentos.");
+                    alert("Has alcanzado el máximo de intentos.");
                 }
             }
         }
     };
-
+    
     usuario.obtenerNombre();
     usuario.validarEdad();
-
+    
     if (usuario.edad < 18) {
-        console.log("Lo sentimos, no puedes continuar debido a tu edad.");
+        alert("Lo sentimos, no puedes continuar debido a tu edad.");
         return;
     }
-
+    
     usuario.obtenerCiudad();
     usuario.tipoDeEntrada();
     usuario.cantidadDeEntradas();
-
+    
     if (!isNaN(usuario.cantidadEntradas) && usuario.cantidadEntradas > 0 && usuario.cantidadEntradas <= 20) {
         usuario.calcularEntradas();
         usuario.confirmarPago();
-
+    
         let usuarioCompleto = {
             nombre: usuario.nombre,
             edad: usuario.edad,
@@ -152,10 +150,10 @@ function DatosUsuario() {
             mail: usuario.mail,
             fecha: usuario.fecha
         };
-
+    
         usuarios.push(usuarioCompleto);
     }
-
+    
     console.log("Usuarios:");
     
     usuarios.forEach(function(usuario) {
